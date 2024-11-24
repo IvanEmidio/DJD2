9ef4cc677579b1c1fe8029c5e5b69bbe7d3243a1
Paulo Silva <pauloalmeida1288@gmail.com>
2024-11-24 14:45:37 +0000

    Adcionados Scripts iniciais do primeiro puzzle

---
+++ Assets/Scripts/FuseBox/Fuse.cs
@@ -0,0 +1,23 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Fuse : MonoBehaviour
+{
+    [SerializeField] private State _state;
+
+    public State state
+    {
+        get { return _state; }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FuseBox/TypeFuse.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+
+public class TypeFuse
+{
+   [Flags]
+    enum State
+    {
+        state = 1 << 0
+    }
+}
