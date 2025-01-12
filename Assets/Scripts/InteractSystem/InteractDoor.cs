fdd11de62ac11cec7f9d37c92803fd8f53acde67
IvanEmidio <ivan.emidio@live.com.pt>
2025-01-12 00:49:26 +0000

    updated interacti script and, fixed some bugs pickup code

---
+++ Assets/Scripts/InteractSystem/InteractDoor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class InteractDoor : Interactable
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("abcd");
+        anim.SetTrigger("Advanced");
+    }
+
+}
